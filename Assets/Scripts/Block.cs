1b5fb268255e917d2fac756a6220340a9aad1394
jgr1585 <jgr1585@students.fhv.at>
2023-11-28 23:30:20 +0100

    Added Removing of Blocks

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,24 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField]
+    private AudioClip explosionClip;
 
-    void OnCollisionEnter(Collision c)
+    private void OnCollisionEnter(Collision c)
     {
-        Debug.Log("Collision");
+        if (!c.gameObject.CompareTag("Ball")) return;
+
+        gameObject.SetActive(false);
+        AudioSource.PlayClipAtPoint(explosionClip, transform.position);
     }
 }
